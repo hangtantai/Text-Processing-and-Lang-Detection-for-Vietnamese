e0b2a0c98a8c1b7cca92ed7c78c61bc1b121eeb7
Hang Tan Tai <133491245+hangtantai@users.noreply.github.com>
2024-10-27 03:14:31 +0700

    Delete test4.py

--- test4.py
+++
@@ -1,6 +0,0 @@
-import torch
-import model_handling
-from data_handling import DataCollatorForNormSeq2Seq
-from model_handling import EncoderDecoderSpokenNorm
-import os
-os.environ["CUDA_VISIBLE_DEVICES"] = ""
